15705c875812fa843c4bb27ec2619c2efae3cb57
pervin agayev <pervinagayev28@gmail.com>
2023-11-08 18:27:14 +0400

    asd

---
+++ ForecastDesign/ViewModels/ViewModelPages/ViewModelEntry.cs
@@ -0,0 +1,14 @@
+﻿using ForecastDesign.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForecastDesign.ViewModels.ViewModelPages
+{
+    public class ViewModelEntry:ServiceINotifyPropertyChanged
+    {
+       
+    }
+}
